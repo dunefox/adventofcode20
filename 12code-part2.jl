f3213b5aad60881c081187344d38daceeddc9c02
dunefox <ph.fuchs@campus.lmu.de>
2020-12-12 17:08:11 +0100

    Julia Part 2 working -> I really have to start reading the task desc. properly... Christ. Runtime P2: 330 μs

--- 12code-part2.jl
+++ 12code-part2.jl
@@ -1,35 +1,25 @@
 using BenchmarkTools, IterTools, Match
 
-input = open(readlines, "12example.txt")
+input = open(readlines, "12input.txt")
 
 # Part 1
 mutable struct Waypoint
     x::Integer
     y::Integer
-    curr::Integer
 end
 
 mutable struct Ship
     x::Integer
     y::Integer
-    # curr::Integer
     waypoint::Waypoint
 end
 
 function rot90(x, y)
-    if (0 < x && 0 < y) || (x < 0 && y < 0)
-        return x, -y
-    elseif (0 < x && y < 0) || (x < 0 && 0 < y)
-        return -x, y
-    else
-        @error "Something is 0" x y
-        error("rot90")
-    end
+    return (y, -x)
 end
 
 function steer!(coeff, val)
     wp = ship.waypoint
-
     val = coeff == 1 ? val : (360 - val)
 
     if val == 90
@@ -44,29 +34,17 @@ function steer!(coeff, val)
         (dx, dy) = rot90(-wp.x, -wp.y)
         wp.x = dx
         wp.y = dy
-    else
-        @error "Something failed" coeff val
-        error("steer!")
     end
 end
 
-    # s = (360 + ship.waypoint.curr + val) % 360
-    
-    # @match ship.curr begin
-    #     90  => move!(w, 0, ship.waypoint)
-    #     180 => move!(0, -w, ship.waypoint)
-    #     270 => move!(-w, 0, ship.waypoint)
-    #     0   => move!(0, w, ship.waypoint)
-    # end
-
-function move!(w)
+function moveship!(w)
     ship.x += w * ship.waypoint.x
     ship.y += w * ship.waypoint.y
 end
 
-function move!(dx, dy, what) # TODO
-    what.x += dx
-    what.y += dy
+function movewp!(dx, dy)
+    ship.waypoint.x += dx
+    ship.waypoint.y += dy
 end
 
 function run1(instr)
@@ -74,22 +52,22 @@ function run1(instr)
     
     @match r begin
         # Move Waypoint
-        'N' => move!(0, w, ship.waypoint)
-        'S' => move!(0, -w, ship.waypoint)
-        'E' => move!(w, 0, ship.waypoint)
-        'W' => move!(-w, 0, ship.waypoint)
+        'N' => movewp!(0, w)
+        'S' => movewp!(0, -w)
+        'E' => movewp!(w, 0)
+        'W' => movewp!(-w, 0)
         # Turn Waypoint
         'R' => steer!(1, w)
         'L' => steer!(-1, w)
         # Move Ship towards Waypoint
-        'F' => move!(w)
+        'F' => moveship!(w)
     end
 end
 
 function part2()
-    global ship = Ship(0, 0, Waypoint(10, 1, 90))
+    global ship = Ship(0, 0, Waypoint(10, 1))
 
-    map(x->run1(x), input)
+    map(x -> run1(x), input)
 
     return abs(ship.x) + abs(ship.y)
 end
