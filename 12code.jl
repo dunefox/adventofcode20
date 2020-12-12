b097e0edcee62cd2247f8b7589c7eb97d1fb0f6e
dunefox <ph.fuchs@campus.lmu.de>
2020-12-12 14:10:46 +0100

    Part 1 Julia: 234 μs

---
+++ 12code.jl
@@ -0,0 +1,60 @@
+using BenchmarkTools, IterTools, Match
+
+input = open(readlines, "12input.txt")
+
+dir2val = Dict(
+    'N' => 360,
+    'E' => 90,
+    'S' => 180,
+    'W' => 270
+)
+
+val2dir = Dict(values(dir2val) .=> keys(dir2val))
+
+mutable struct Ship
+    x
+    y
+    curr
+end
+
+function steer!(val)
+    ship.curr = (360 + ship.curr + val) % 360 # TODO
+end
+
+function move!(w)
+    @match ship.curr begin
+        90 => move!(w, 0)
+        180 => move!(0, -w)
+        270 => move!(-w, 0)
+        0 => move!(0, w)
+    end
+end
+
+function move!(dx, dy)
+    ship.x += dx
+    ship.y += dy
+end
+
+function run1(instr)
+    r, w = instr[1], parse(Int, instr[2:end])
+    
+    @match r begin
+        'N' => move!(0, w)
+        'S' => move!(0, -w)
+        'E' => move!(w, 0)
+        'W' => move!(-w, 0)
+        'R' => steer!(w)
+        'L' => steer!(-w)
+        'F' => move!(w)
+    end
+end
+
+function part1()
+    global ship = Ship(0, 0, 90)
+
+    map(x->run1(x), input)
+
+    return abs(ship.x) + abs(ship.y)
+end
+
+part1()
