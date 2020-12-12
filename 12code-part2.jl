f0da89f7993b71bdcc16bc37ae4c7aef08a77236
dunefox <ph.fuchs@campus.lmu.de>
2020-12-12 16:24:41 +0100

    Julia Part 2: working for example

--- 12code-part2.jl
+++ 12code-part2.jl
@@ -16,12 +16,41 @@ mutable struct Ship
     waypoint::Waypoint
 end
 
+function rot90(x, y)
+    if (0 < x && 0 < y) || (x < 0 && y < 0)
+        return x, -y
+    elseif (0 < x && y < 0) || (x < 0 && 0 < y)
+        return -x, y
+    else
+        @error "Something is 0" x y
+        error("rot90")
+    end
+end
+
 function steer!(coeff, val)
     wp = ship.waypoint
-    if 0 < 
 
+    val = coeff == 1 ? val : (360 - val)
 
-    # s = (360 + ship.waypoint.curr + val) % 360 # TODO
+    if val == 90
+        (dx, dy) = rot90(wp.x, wp.y)
+        wp.x = dx
+        wp.y = dy
+    elseif val == 180
+        (dx, dy) = rot90(rot90(wp.x, wp.y)...)
+        wp.x = dx
+        wp.y = dy
+    elseif val == 270
+        (dx, dy) = rot90(-wp.x, -wp.y)
+        wp.x = dx
+        wp.y = dy
+    else
+        @error "Something failed" coeff val
+        error("steer!")
+    end
+end
+
+    # s = (360 + ship.waypoint.curr + val) % 360
     
     # @match ship.curr begin
     #     90  => move!(w, 0, ship.waypoint)
@@ -29,14 +58,13 @@ function steer!(coeff, val)
     #     270 => move!(-w, 0, ship.waypoint)
     #     0   => move!(0, w, ship.waypoint)
     # end
-end
 
 function move!(w)
     ship.x += w * ship.waypoint.x
     ship.y += w * ship.waypoint.y
 end
 
-function move!(dx, dy, what)
+function move!(dx, dy, what) # TODO
     what.x += dx
     what.y += dy
 end
@@ -52,16 +80,18 @@ function run1(instr)
         'W' => move!(-w, 0, ship.waypoint)
         # Turn Waypoint
         'R' => steer!(1, w)
-        'L' => steer!(-1, -w)
+        'L' => steer!(-1, w)
         # Move Ship towards Waypoint
         'F' => move!(w)
     end
 end
 
-function part1()
+function part2()
     global ship = Ship(0, 0, Waypoint(10, 1, 90))
 
     map(x->run1(x), input)
 
     return abs(ship.x) + abs(ship.y)
 end
+
+part2()
