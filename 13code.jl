884bb438adb5a007fdbe7ed3b17a5cfcd09be4e9
dunefox <ph.fuchs@campus.lmu.de>
2020-12-14 00:11:32 +0100

    Julia Part 2 was quite dfficult, but in the end I got it (with RC): runtime ~14.5 μs

--- 13code.jl
+++ 13code.jl
@@ -8,44 +8,40 @@ depart, buses = open("13input.txt") do f
     depart, buses
 end
 
-function part1(depart, buses)
+function solve_part1(depart, buses)
     lowest = buses .- mod.(depart, buses)
     buses[argmin(lowest)] * minimum(lowest)
 end
 
-@btime part1(depart, buses)
+@btime solve_part1(depart, buses)
 
 # Part 2
-# Chinese remainder problem, etc. ... no idea. 🤷
-
-# This code works but I couldn't come up with my own solution => "part2" is taken from RC
-
-function prep(buses)
-    rests = (x->(x[2], x[1] - 1)).(filter(x->x[2] != "x", collect(enumerate(buses))))
-    tmp = [0; abs.([el[2] for el in rests[2:end]] .- [el[1] for el in rests[2:end]])]
-    
-    return [x[1] for x in rests], tmp
+busplan = open("13input.txt") do f
+    _, snd = readlines(f)
+    [x != "x" ? parse(BigInt, x) : x for x in split(snd, ",")]
 end
 
-buses = open("13input.txt") do f
-    _, snd = readlines(f)
-    buses = [x != "x" ? parse(Int, x) : x for x in split(snd, ",")]
-    prep(buses)
+function crunch_numbers(buses)
+    parsed = filter(x -> x[2] != "x", collect(enumerate(buses)))
+    modulos = map(x -> x[2], parsed)
+    remainders = map(x -> x[2] - (x[1] - 1), parsed)
+
+    modulos, remainders
 end
 
 # Taken from https://rosettacode.org/wiki/Chinese_remainder_theorem#Julia
-function part2(n::Array, a::Array)
+function solve_part2(n::Array, a::Array)
     Π = prod(n)
     mod(sum(ai * invmod(Π ÷ ni, ni) * Π ÷ ni for (ni, ai) in zip(n, a)), Π)
 end
 
 # Tests
 @testset "Examples" begin
-    @test part2(prep([17, "x", 13, 19])...)    == 3417
-    @test part2(prep([67, 7, 59, 61])...)      == 754018
-    @test part2(prep([67, "x", 7, 59, 61])...) == 779210
-    @test part2(prep([67, 7, "x", 59, 61])...) == 1261476
-    @test part2(prep([1789, 37, 47, 1889])...) == 1202161486
+    @test solve_part2(crunch_numbers([17, "x", 13, 19])...)    == 3417
+    @test solve_part2(crunch_numbers([67, 7, 59, 61])...)      == 754018
+    @test solve_part2(crunch_numbers([67, "x", 7, 59, 61])...) == 779210
+    @test solve_part2(crunch_numbers([67, 7, "x", 59, 61])...) == 1261476
+    @test solve_part2(crunch_numbers([1789, 37, 47, 1889])...) == 1202161486
 end;
 
-@btime part2(buses...)
+@btime solve_part2(crunch_numbers(busplan)...)
